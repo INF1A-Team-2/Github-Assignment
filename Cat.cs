73dc437e5a2d96f00faab9e24cecdeba0f91fe4f
PieterLandaal <0981786@hr.nl>
2023-02-09 14:36:27 +0100

    New class

---
+++ Cat.cs
@@ -0,0 +1,12 @@
+﻿public class Cat
+{
+    public string Name;
+    public Cat(string name)
+    {
+        this.Name = name;
+    }
+    public void Meow()
+    {
+        Console.WriteLine($"Cat {Name} says meow");
+    }
+}
