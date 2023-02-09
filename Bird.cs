56ece5f3fd98e215ee7c8640554ff69605efa6d3
Joost-Reintjes <95181999+Joost-Reintjes@users.noreply.github.com>
2023-02-09 14:53:28 +0100

    Update Bird.cs
    fixed constructor

--- Bird.cs
+++ Bird.cs
@@ -1,5 +1,13 @@
-﻿class Bird
+﻿class Bird       
 {
+    public string Name;
+
+    public Bird(string name)
+    {
+        this.Name = name;
+    }
+     
+          
     public void Chirp()
     {
         Console.WriteLine("cheep cheep 9/11 was an inside job cheep");
