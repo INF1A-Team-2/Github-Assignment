f71dc83d4ce0411f494b76c78c86c137108bd48f
itsmorbintime123 <124811893+itsmorbintime123@users.noreply.github.com>
2023-02-09 14:56:01 +0100

    snake class

---
+++ Snake_class.cs
@@ -0,0 +1,16 @@
+class Snake
+
+{
+
+    public string Name;
+    public Snake(string name)
+{
+    this.Name = name;
+}
+
+    public void Hiss()
+    {
+        Console.WriteLine("Hiss");
+    }
+}
+
